b16dc6774da8e2a926a2d081efdf8d274e9ccc04
Alexey Saulin <yabaunti@gmail.com>
2022-03-13 12:24:32 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
+'use strict'
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
+  }
+
   return arr; // array
 }
 
