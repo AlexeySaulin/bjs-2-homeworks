41d4d57d6e40ed2fe85c025bd6da081dfda99518
Alexey Saulin <yabaunti@gmail.com>
2022-03-17 20:53:07 +0300

    first commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,70 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
+
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum =0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
 
-  // Ваш кода
-  // for ...
+  let max = -Infinity;
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+
+    let result = func(arrOfArr[i]);
+
+    if (result > max) {
+
+      max = result;
+
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max;
+  min = Infinity;
+  max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return max - min;
 }
