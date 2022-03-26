a3ffde30b4f3466228f18ae7332dfd6ecf8c40b2
Alexey Saulin <yabaunti@gmail.com>
2022-03-27 00:34:09 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,30 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
-
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
-
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+    this.marks = [mark]; 
+    } else {
+      this.marks.push(mark);
+    }
+}
+Student.prototype.addMarks = function (...mark) {
+  this.marks.push(...mark);
+}
+Student.prototype.getAverage = function (marks) {
+  let sum = 0;
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+  return (sum / marks.length);
+  }
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
